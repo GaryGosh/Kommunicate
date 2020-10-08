2f4cea08f8339d1d5047f1e3d927284dc8697f19
GaryGosh <clichethe1@yahoo.com>
2020-10-08 23:44:04 +0530

    search fix

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search({ user }) {
-
+  const[filteredItem, setFilteredItem] = useState([]);
   const searchUSer = (e) => {
     /* console.log(e.target.value); */
     let val = e.target.value;
-    return user.map((user) => (
-        user.first_name === val
-    ))
+    let filterData = user.filter((user) => (
+      user.first_name.toLowerCase().includes(val.toLowerCase())
+    ));
+    setFilteredItem(filterData)
   };
-  
+  console.log('filter', filteredItem);
   return (
     <div>
       <input type="text" placeholder="Search user" onChange={searchUSer} />
+      {filteredItem?.map(({avatar, id, first_name, last_name, email}) => (
+                <tr>
+                    <td><img src={avatar} alt="user dp"/></td>
+                    <td>{id}</td>
+                    <td>{first_name}</td>
+                    <td>{last_name}</td>
+                    <td>{email}</td>
+                </tr>
+            ) )}
     </div>
   );
 }
